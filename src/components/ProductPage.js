1c526a88086dc161a5ccd5a8622f46a51c1e72e3
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2023-03-02 13:45:34 +0400

    productpage update

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -5,11 +5,13 @@ import { sendEmail } from "./sendEmail";
 import TimestampConverter from "./TimeStampConverter";
 const ProductPage = (props) => {
   const [showCode,setShowCode]=useState(false)
-  const [product,setProduct] = useState({})
+  const [product,setProduct] = useState([])
   let state = useLocation();
   const showLogin = props.showLogin
   const setShowLogin = props.setShowLogin
   const id = state.state.product.id;
+  const coverUrl = state.state.product.attributes.coverUrl
+  const endDate = state.state.product.attributes.endDate
   useEffect(()=>{
     var requestOptions = {
       method: 'GET',
@@ -26,6 +28,7 @@ const ProductPage = (props) => {
   const handleClick=(e)=>{
     setShowLogin(prevState =>!prevState)
   }
+  console.log(product)
   const handleGetCode = ()=>{
     setShowCode(true)
      setRandomCode(Math.floor(100000 + Math.random() * 900000));
@@ -37,20 +40,19 @@ const ProductPage = (props) => {
   return (
     
     <div className="flex justify-center flex-col items-center bg-white p-6">
-      <div>{product.company}</div>
+      <div className={styles.company}>{product.company}</div>
       <div
+          className={styles.cover}
         style={{
-          background: `url(${product.coverUrl})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
+          background: `url(${coverUrl})`,
+ 
         }}
-        className={styles.cover}
       ></div>
 
       <div className={styles.info}>
         <div className={styles.productName}>{product.productName}   {product.description}</div>
     
-        <div><TimestampConverter timestamp={Date.parse(product.endDate)}/></div>
+        <div><TimestampConverter timestamp={Date.parse(endDate)}/></div>
       </div>
       {token? <div className={styles.getCode} onClick={handleGetCode}>კოდის მიღება</div>:<div className={styles.register}>გთხოვთ გაიარეთ <button onClick={handleClick}>ავტორიზაცია</button></div>}
       {showCode && <div>
