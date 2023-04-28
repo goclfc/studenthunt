0d6cb9bd98f869c5a2af4380b0123ac1c6b3b8e4
Henry <61660956+goclfc@users.noreply.github.com>
2023-04-28 14:14:08 +0400

    upd product

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,11 @@
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 import { useFetchedData } from '../../services/HttpServices'
 import './banner.css'
 const Banner = () => {
-  const data = useFetchedData('baners')
+  const [data,setData ]= useState(useFetchedData('baners'))
   const prodId = data[0]?.attributes.products.data[0].id
+  console.log(data)
   return (
     <div className='flex flex-col items-center'>
       <Link to={`products/${prodId}`} state={prodId}>
